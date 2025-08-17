92e3329db0fe006085914101f64bbc6365eec0ac
Ethaniel <ethamorim@gmail.com>
2025-08-17 16:22:42 -0300

    fix: use new model of deferring reply

--- src/main/kotlin/br/ethamorim/discordbot/auroramusic/event/command/WeeklyAlbumStartCommandExecutor.kt
+++ src/main/kotlin/br/ethamorim/discordbot/auroramusic/event/command/WeeklyAlbumStartCommandExecutor.kt
@@ -5,10 +5,13 @@ import br.ethamorim.discordbot.auroramusic.redis.entity.GuildMember
 import br.ethamorim.discordbot.auroramusic.redis.entity.WeeklyAlbumCycle
 import br.ethamorim.discordbot.auroramusic.redis.repository.GuildMemberRepository
 import br.ethamorim.discordbot.auroramusic.redis.repository.WeeklyAlbumCycleRepository
+import br.ethamorim.discordbot.auroramusic.util.LoggerUtil
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import discord4j.core.spec.InteractionFollowupCreateMono
+import org.slf4j.Logger
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import reactor.core.scheduler.Schedulers
 import java.util.*
 
 @Component
@@ -16,41 +19,54 @@ class WeeklyAlbumStartCommandExecutor(
     private val guildMemberRepository: GuildMemberRepository,
     private val weeklyAlbumCycleRepository: WeeklyAlbumCycleRepository
 ) : CommandExecutor<ChatInputInteractionEvent> {
+
+    private val log = LoggerUtil.getLogger(WeeklyAlbumStartCommandExecutor::class.java)
+
     override fun execute(event: ChatInputInteractionEvent): Mono<Void> {
-        try {
-            val orderToStart = event.getOption("order")
-                .flatMap { it.value }
-                .map {
-                    val value = it.asString()
-                    value.toInt()
-                }
-                .orElseThrow { throw InvalidOptionException("Ordem inválida!") }
-
-            val guildMember = findByWeeklyAlbumOrder(orderToStart)
-                .orElseThrow { throw InvalidOptionException("Ordem inválida!") }
-
-            val cycleOptional = weeklyAlbumCycleRepository.findById(0)
-            val cycle = if (cycleOptional.isPresent) {
-                val c = cycleOptional.get().copy()
-                weeklyAlbumCycleRepository.deleteById(0)
-
-                c.currentMemberId = guildMember.id
-                c.isOn = true
-                c
-            } else {
-                WeeklyAlbumCycle(0, guildMember.id, true)
+        return Mono.fromCallable { processNextMemberInteration(event) }
+            .subscribeOn(Schedulers.boundedElastic())
+            .flatMap { nickname ->
+                event.createFollowup("Tá bom! Começarei a contar o ciclo a partir de $nickname :)")
             }
-            weeklyAlbumCycleRepository.save(cycle)
+            .onErrorResume { error ->
+                val errorMessage = error.message!!
+                log.error("Error trying to start a new cycle: $errorMessage")
 
-            return event.createFollowup("Começarei a contar o ciclo a partir de ${guildMember.nickname}")
-                .then()
-        } catch (e: InvalidOptionException) {
-            return event.createFollowup(e.message!!)
-                .then()
-        }
+                event.createFollowup("Não estou me sentindo muito bem :(")
+            }
+            .then()
     }
 
-    fun findByWeeklyAlbumOrder(order: Int): Optional<GuildMember> {
+    private fun processNextMemberInteration(event: ChatInputInteractionEvent): String {
+        val orderToStart = event.getOption("order")
+            .flatMap { it.value }
+            .map {
+                val value = it.asString()
+                value.toInt()
+            }
+            .orElseThrow { throw InvalidOptionException("Ordem inválida!") }
+
+        val guildMember = findByWeeklyAlbumOrder(orderToStart)
+            .orElseThrow { throw InvalidOptionException("Ordem inválida!") }
+
+        val cycleOptional = weeklyAlbumCycleRepository.findById(0)
+        val cycle = if (cycleOptional.isPresent) {
+            val c = cycleOptional.get().copy()
+            weeklyAlbumCycleRepository.deleteById(0)
+
+            c.currentMemberId = guildMember.id
+            c.isOn = true
+            c
+        } else {
+            WeeklyAlbumCycle(0, guildMember.id, true)
+        }
+        weeklyAlbumCycleRepository.save(cycle)
+        log.info("New cycle start saved succesfully")
+
+        return guildMember.nickname
+    }
+
+    private fun findByWeeklyAlbumOrder(order: Int): Optional<GuildMember> {
         val member = guildMemberRepository.findAll().filter { it.weeklyAlbumOrder == order }
         if (member.isEmpty())
             return Optional.empty()
--- src/main/kotlin/br/ethamorim/discordbot/auroramusic/event/listener/CommandListener.kt
+++ src/main/kotlin/br/ethamorim/discordbot/auroramusic/event/listener/CommandListener.kt
@@ -2,20 +2,35 @@ package br.ethamorim.discordbot.auroramusic.event.listener
 
 import br.ethamorim.discordbot.auroramusic.event.command.Command.START_ALBUM_CYCLE
 import br.ethamorim.discordbot.auroramusic.event.command.WeeklyAlbumStartCommandExecutor
+import br.ethamorim.discordbot.auroramusic.util.LoggerUtil
 import discord4j.core.GatewayDiscordClient
 import discord4j.core.event.domain.interaction.ChatInputInteractionEvent
 import org.springframework.stereotype.Component
 import reactor.core.publisher.Mono
+import java.time.Duration
 
 @Component
 class CommandListener(gatewayClient: GatewayDiscordClient, weeklyAlbumStartCommandExecutor: WeeklyAlbumStartCommandExecutor) {
+
+    private val log = LoggerUtil.getLogger(CommandListener::class.java)
+
     init {
         gatewayClient.on(ChatInputInteractionEvent::class.java) { event ->
-            if (START_ALBUM_CYCLE.equalsCommandName(event.commandName))
+            log.info("Command ${event.commandName} received. Deferring reply.")
+            if (START_ALBUM_CYCLE.equalsCommandName(event.commandName)) {
                 event.deferReply()
                     .then(weeklyAlbumStartCommandExecutor.execute(event))
-            else
-                Mono.empty<Void>()
+                    .timeout(Duration.ofSeconds(15))
+                    .doOnError { error ->
+                        log.error("Command execution error: ${error.message}")
+                        event.createFollowup("Não fala comigo agora >:(")
+                            .onErrorResume { Mono.empty() }
+                            .subscribe()
+                    }
+                    .subscribe()
+            }
+
+            Mono.empty<Void>()
         }.subscribe()
     }
 }
